f40a5105f79caebdf1e594abe74eb3addd00709a
Alex Salcedo <awsalcedo@sri.gob.ec>
2023-01-18 14:01:04 -0500

    Implementation Dependency Injection with Dagger Hilt

--- app/src/main/java/com/asalcedo/jetpackcomposeinstagram/MainActivity.kt
+++ app/src/main/java/com/asalcedo/jetpackcomposeinstagram/MainActivity.kt
@@ -3,6 +3,7 @@ package com.asalcedo.jetpackcomposeinstagram
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,8 +14,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.asalcedo.jetpackcomposeinstagram.login.ui.LoginScreen
 import com.asalcedo.jetpackcomposeinstagram.login.ui.LoginViewModel
 import com.asalcedo.jetpackcomposeinstagram.ui.theme.JetpackComposeInstagramTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val loginViewModel: LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,7 +31,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     /*TODO hacer con inyección de dependencias*/
-                    LoginScreen(LoginViewModel())
+                    //LoginScreen(LoginViewModel())
+                    LoginScreen(loginViewModel)
                 }
             }
         }
--- app/src/main/java/com/asalcedo/jetpackcomposeinstagram/login/domain/LoginUseCase.kt
+++ app/src/main/java/com/asalcedo/jetpackcomposeinstagram/login/domain/LoginUseCase.kt
@@ -1,9 +1,10 @@
 package com.asalcedo.jetpackcomposeinstagram.login.domain
 
 import com.asalcedo.jetpackcomposeinstagram.login.data.LoginRepository
+import javax.inject.Inject
 
-class LoginUseCase {
-    private val repository = LoginRepository()
+class LoginUseCase @Inject constructor(private val repository: LoginRepository) {
+    //private val repository = LoginRepository()
 
     suspend operator fun invoke(user: String, password: String): Boolean {
         return repository.doLogin(user, password)
